00e7df72b5299b07e906d17102253cffd2fc8d1b
victor-singha <vicunite2@gmail.com>
2021-04-21 16:09:59 +0530

    [victor] take input from console

--- Address-Book/Program.cs
+++ Address-Book/Program.cs
@@ -9,9 +9,27 @@ namespace Address_Book
             Console.WriteLine("Welcome To Address Book");
             Console.WriteLine("-----------------------");
             Console.WriteLine("-----------------------");
+            Console.WriteLine("--Add Contact Details--");
 
-            ContactOperation contactOperation = new ContactOperation();
-            contactOperation.addContact("victor", "singha", "addrezzz", "cityx", "statez", "766452", "8756473625", "victor@gmail.com");
+            Console.WriteLine("First Name:");
+            string firstName = Console.ReadLine();
+            Console.WriteLine("Last Name:");
+            string lastName = Console.ReadLine();
+            Console.WriteLine("Address:");
+            string address = Console.ReadLine();
+            Console.WriteLine("City:");
+            string city = Console.ReadLine();
+            Console.WriteLine("State:");
+            string state = Console.ReadLine();
+            Console.WriteLine("Zip Code:");
+            string zip = Console.ReadLine();
+            Console.WriteLine("Phone Number:");
+            string phoneNumber = Console.ReadLine();
+            Console.WriteLine("Email Address:");
+            string email = Console.ReadLine();
+
+        ContactOperation contactOperation = new ContactOperation();
+            contactOperation.addContact(firstName, lastName, address, city, state, zip, phoneNumber, email);
             contactOperation.showList();
 
             Console.ReadKey();
