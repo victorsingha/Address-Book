4ae379fe7f7123a06d2d9cf58bb085e8c95b460e
victor-singha <vicunite2@gmail.com>
2021-04-21 14:15:00 +0530

    [victor] addded contact model and operation class

---
+++ Address-Book/Contact.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Address_Book
+{
+    public class Contact
+    {
+        private String firstName;
+        private String lastName;
+        private String address;
+        private String city;
+        private String state;
+        private String zip;
+        private String phoneNumber;
+        private String email;
+
+        public Contact(string firstName, string lastName, string address, string city, string state, string zip, string phoneNumber, string email)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Address = address;
+            this.City = city;
+            this.State = state;
+            this.Zip = zip;
+            this.PhoneNumber = phoneNumber;
+            this.Email = email;
+        }
+
+        public string FirstName { get => firstName; set => firstName = value; }
+        public string LastName { get => lastName; set => lastName = value; }
+        public string Address { get => address; set => address = value; }
+        public string City { get => city; set => city = value; }
+        public string State { get => state; set => state = value; }
+        public string Zip { get => zip; set => zip = value; }
+        public string PhoneNumber { get => phoneNumber; set => phoneNumber = value; }
+        public string Email { get => email; set => email = value; }
+    }
+}
--- Address-Book/Program.cs
+++ Address-Book/Program.cs
@@ -7,6 +7,14 @@ namespace Address_Book
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome To Address Book");
+            Console.WriteLine("-----------------------");
+            Console.WriteLine("-----------------------");
+
+            ContactOperation contactOperation = new ContactOperation();
+            contactOperation.addContact("victor", "singha", "addrezzz", "cityx", "statez", "766452", "8756473625", "victor@gmail.com");
+            contactOperation.showList();
+
+            Console.ReadKey();
         }
     }
 }
