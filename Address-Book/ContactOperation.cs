b055c21d6d0321a500135eba6fce5f9d59c3b2e1
victor-singha <vicunite2@gmail.com>
2021-04-30 15:58:11 +0530

    [victor] refactor: used Dictionary instead of List

--- Address-Book/ContactOperation.cs
+++ Address-Book/ContactOperation.cs
@@ -6,89 +6,85 @@ namespace Address_Book
 {
     public class ContactOperation
     {
-        private List<Contact> ContactList;
-        public ContactOperation()
-        {
-            this.ContactList = new List<Contact>();
-        }
-
+        private Dictionary<string, Contact> ContactLists = new Dictionary<string,Contact>();
         public void addContact(string firstName, string lastName, string address, string city, string state, string zip, string phoneNumber, string email)
         {
             Contact newContact = new Contact(firstName, lastName, address, city, state, zip, phoneNumber, email);
-            this.ContactList.Add(newContact);
+            //this.ContactList.Add(newContact);
+            this.ContactLists.Add(firstName, newContact);
         }
         public void showList()
         {
-           foreach(var contact in ContactList)
+           foreach(var contact in ContactLists)
             {
                 Console.WriteLine("#####################");
-                Console.WriteLine("FirstName: "+contact.FirstName);
-                Console.WriteLine("LastName: "+contact.LastName);
-                Console.WriteLine("Address: "+contact.Address);
-                Console.WriteLine("City: "+contact.City);
-                Console.WriteLine("State: "+contact.State);
-                Console.WriteLine("ZipCode: "+contact.Zip);
-                Console.WriteLine("Phone Number: "+contact.PhoneNumber);
-                Console.WriteLine("Email: "+contact.Email);
+                Console.WriteLine("FirstName: "+contact.Value.FirstName);
+                Console.WriteLine("LastName: "+contact.Value.LastName);
+                Console.WriteLine("Address: "+contact.Value.Address);
+                Console.WriteLine("City: "+contact.Value.City);
+                Console.WriteLine("State: "+contact.Value.State);
+                Console.WriteLine("ZipCode: "+contact.Value.Zip);
+                Console.WriteLine("Phone Number: "+contact.Value.PhoneNumber);
+                Console.WriteLine("Email: "+contact.Value.Email);
                 Console.WriteLine("#####################");
 
             }
         }
         public void editContact(string fName)
         {
-            foreach(var contact in ContactList)
+            foreach(var contact in ContactLists)
             {
-                if(contact.FirstName == fName)
+                if(contact.Key == fName)
                 {
-                    Console.WriteLine("Edit? Old FirstName: " + contact.FirstName);
+                    Console.WriteLine("Edit? Old FirstName: " + contact.Value.FirstName);
                     string newFirstName = Console.ReadLine();
-                    Console.WriteLine("Edit? Old LastName: " + contact.LastName);
+                    Console.WriteLine("Edit? Old LastName: " + contact.Value.LastName);
                     string newLastName = Console.ReadLine();
-                    Console.WriteLine("Edit? Old Address: " + contact.Address);
+                    Console.WriteLine("Edit? Old Address: " + contact.Value.Address);
                     string newAddress = Console.ReadLine();
-                    Console.WriteLine("Edit? Old City: " + contact.City);
+                    Console.WriteLine("Edit? Old City: " + contact.Value.City);
                     string newCity = Console.ReadLine();
-                    Console.WriteLine("Edit? Old State: " + contact.State);
+                    Console.WriteLine("Edit? Old State: " + contact.Value.State);
                     string newState = Console.ReadLine();
-                    Console.WriteLine("Edit? Old ZipCode: " + contact.Zip);
+                    Console.WriteLine("Edit? Old ZipCode: " + contact.Value.Zip);
                     string newZip = Console.ReadLine();
-                    Console.WriteLine("Edit? Old Phone Number: " + contact.PhoneNumber);
+                    Console.WriteLine("Edit? Old Phone Number: " + contact.Value.PhoneNumber);
                     string newPhoneNumber = Console.ReadLine();
-                    Console.WriteLine("Edit? Old Email: " + contact.Email);
+                    Console.WriteLine("Edit? Old Email: " + contact.Value.Email);
                     string newEmail = Console.ReadLine();
                     Console.WriteLine("-----------------");
 
                     if(newFirstName != "")
                     {
-                        contact.FirstName = newFirstName;
+                        contact.Value.FirstName = newFirstName;
                     }
                     if (newLastName != "")
                     {
-                        contact.LastName = newLastName;
+                        contact.Value.LastName = newLastName;
                     }
                     if (newAddress != "")
                     {
-                        contact.Address = newAddress;
+                        contact.Value.Address = newAddress;
                     }
                     if (newCity != "")
                     {
-                        contact.City = newCity;
+                        contact.Value.City = newCity;
                     }
                     if (newState != "")
                     {
-                        contact.State = newState;
+                        contact.Value.State = newState;
                     }
                     if (newZip != "")
                     {
-                        contact.Zip = newZip;
+                        contact.Value.Zip = newZip;
                     }
                     if (newPhoneNumber != "")
                     {
-                        contact.PhoneNumber = newPhoneNumber;
+                        contact.Value.PhoneNumber = newPhoneNumber;
                     }
                     if (newEmail != "")
                     {
-                        contact.Email = newEmail;
+                        contact.Value.Email = newEmail;
                     }
                 }
             }
@@ -96,11 +92,11 @@ namespace Address_Book
         }
         public void deleteContact(string fname)
         {
-            foreach(var contact in ContactList)
+            foreach(var contact in ContactLists)
             {
-                if (contact.FirstName == fname)
+                if (contact.Key == fname)
                 {
-                    ContactList.Remove(contact);
+                    ContactLists.Remove(contact.Key);
                     Console.WriteLine("Contact Deleted Successfully.");
                     break;
                 }
